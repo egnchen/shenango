58b7d08597d306d264231f5f830a2ab82907e87f
eugene chen <1229522864@qq.com>
2022-09-13 01:41:08 +0800

    update init script

--- init.sh
+++ init.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-# Initialize dpdk module
+# Initialize dpdk & libbpf module
 git submodule init
 git submodule update --recursive
 
 # Apply driver patches
 patch -p 1 -d dpdk/ < ixgbe_19_11.patch
-
 
 if lspci | grep -q 'ConnectX-5'; then
    patch -p 1 -d dpdk/ < mlx5_19_11.patch
@@ -17,6 +16,17 @@ elif lspci | grep -q 'ConnectX-3'; then
     sed -i 's/CONFIG_RTE_LIBRTE_MLX4_PMD=n/CONFIG_RTE_LIBRTE_MLX4_PMD=y/g' dpdk/config/common_base
 fi
 
+# Apply general patches
+patch -p 1 -d dpdk/ < dpdk1911_linux59.patch
+patch -p 1 -d libbpf/ < libbpf_override_regs.patch
+
 # Configure/compile dpdk
 make -C dpdk/ config T=x86_64-native-linuxapp-gcc
 make -C dpdk/ -j`nproc`
+
+# compile libbpf
+cd libbpf
+mkdir build
+cd build
+make -C ../src DESTDIR=$(pwd) INCLUDEDIR= LIBDIR= UAPIDIR= install
+cd ..
